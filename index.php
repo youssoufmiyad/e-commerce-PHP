5c7ba996888e0a818877b18f994ecb4b3da5d444
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-01-21 12:55:14 +0100

    setup front

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>e-commerce PHP</title>
+    <link rel="stylesheet" href="CSS/main.css" type="text/css">
+</head>
+
+
+<body>
+
+    <div class="title">
+        <h1>E-commerce php</h1>
+    </div>
+
+</body>
+
+</html>
