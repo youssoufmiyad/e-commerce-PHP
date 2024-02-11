8b9bac0e89b37e75a9430d83b440c390cea7e556
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-02-11 12:27:36 +0100

    correction verification de l'existence du dossier

--- generate_product_page.php
+++ generate_product_page.php
@@ -10,17 +10,17 @@ $json = [];
 if ($products->num_rows > 0) {
     // output data of each row
     while ($row = $products->fetch_assoc()) {
-        if (!file_exists($row["ProductId"] . "/index.php")) {
+        if (!file_exists("product/" . $row["ProductId"] . "/index.php")) {
             mkdir("product/" . $row["ProductId"] . "/", 0777, true);
             $file = fopen("product/" . $row["ProductId"] . "/index.php", "w") or die("Unable to open file!");
-            writeTemplate($file, $row["Name"], $row["Price"], $row["Vendor"], $row["Quantity"]);
+            productDetailTemplate($file, $row["Name"], $row["Price"], $row["Vendor"], $row["Quantity"]);
             fclose($file);
         }
         array_push($json, $row);
     }
 }
 
-function writeTemplate($file, $name, $price, $vendor, $quantity)
+function productDetailTemplate($file, $name, $price, $vendor, $quantity)
 {
     fwrite($file, "
     <!DOCTYPE html>
