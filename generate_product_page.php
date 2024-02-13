83b266eabf820502ac212efbd570efee9dffc1ed
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-02-13 15:10:24 +0100

    form d'ajout au panier (quantité du produit a renseigner + bouton submit)

--- generate_product_page.php
+++ generate_product_page.php
@@ -39,8 +39,17 @@ function productDetailTemplate($file, $name, $price, $vendor, $quantity, $image)
         <div class=\"product-vendor\">$vendor </div>
         <div class=\"product-quantity\">$quantity </div>
         <div class=\"product-image\"><img src=\"$image\" alt=\"product image\"/></div>
+        <form action=\"../add_to_cart.php\" method=\"post\">
+        <label for=\"cart-quantity\">combien ?</label>
+        <input type=\"number\" name=\"cart-quantity\" id=\"cart-quantity\">
+
+        <!-- export du nom du produit (caché du client grace à l'attribut \"hidden\") -->
+        <input type=\"text\" name=\"product-name\" id=\"product-name\" value=\"$name\" hidden>
+        <input type=\"number\" name=\"product-id\" id=\"product-id\" value=<?php echo basename(__DIR__)?> hidden>
+        <input type=\"submit\" value=\"ajouter au panier\">
+    </form>
     </body>
-    </html>
+    </html> 
     ");
 
 
