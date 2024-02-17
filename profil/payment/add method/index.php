16cd07707bbbff79469d00434c4d84f42bd178f8
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-02-17 14:59:44 +0100

    ajout fonctionnel (3 types de cartes testés)

--- profil/payment/add method/index.php
+++ profil/payment/add method/index.php
@@ -1,9 +1,51 @@
 <?php
+require_once("../../../utils/connect.php");
+
 session_start();
 
-$VISA_REGEX = "^4[0-9]{6,}$";
-$MASTERCARD_REGEX = "^5[1-5][0-9]{5,}|222[1-9][0-9]{3,}|22[3-9][0-9]{4,}|2[3-6][0-9]{5,}|27[01][0-9]{4,}|2720[0-9]{3,}$";
-$AMERICAN_EXPRESS_REGEX = "^3[47][0-9]{5,}$";
+$VISA_REGEX = "^4[0-9]{6,}$^";
+$MASTERCARD_REGEX = "^5[1-5][0-9]{5,}|222[1-9][0-9]{3,}|22[3-9][0-9]{4,}|2[3-6][0-9]{5,}|27[01][0-9]{4,}|2720[0-9]{3,}$^";
+$AMERICAN_EXPRESS_REGEX = "^3[47][0-9]{5,}$^";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    if (!empty($_POST['form-card-number']) && !empty($_POST['form-card-expiration-date']) && !empty($_POST['form-card-cvv'])) {
+
+        $card_number = str_replace(" ", "", $_POST['form-card-number']);
+        $card_date = str_replace("-", "/", $_POST['form-card-expiration-date']);
+        $card_cvv = $_POST['form-card-cvv'];
+
+        if (strlen($card_number) < 15 || strlen($card_number) > 16) {
+            echo "numéro de carte invalide";
+        } elseif (strlen($card_cvv) > 4) {
+            echo "cvv invalide";
+        } else {
+            if (filter_var($card_number, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => $MASTERCARD_REGEX)))) {
+                $card_type = "Mastercard";
+            } else if (filter_var($card_number, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => $AMERICAN_EXPRESS_REGEX)))) {
+                $card_type = "American Express";
+            } else if (filter_var($card_number, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => $VISA_REGEX)))) {
+                $card_type = "VISA";
+            } else {
+                echo "card not supported";
+                return;
+            }
+            if ($card_type) {
+                $addCard = $db->prepare("INSERT INTO `payment` (`UserId`, `CardType`, `CardNumber`, `ExpirationDate`, `CVV` ) VALUES (?,?,?,?,?);");
+                $addCard->bind_param("issss", $_SESSION["user"]["userId"], $card_type, $card_number, $card_date, $card_cvv);
+                $addCard->execute();
+                echo "card added";
+            }
+
+        }
+
+
+
+
+    } else {
+        echo "incomplet";
+    }
+}
 
 ?>
 <!DOCTYPE html>
