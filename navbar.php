a339a9c6d03343c820a9398e496897acb7fa966b
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-02-22 19:17:23 +0100

    lien de connexion plutot qu'inscription

--- navbar.php
+++ navbar.php
@@ -3,6 +3,6 @@
             <li><a href="http://localhost/e-commerce-PHP/products">Marketplace</a></li>
             <li><a href="http://localhost/e-commerce-PHP/">A propos</a></li>
             <li><a href="http://localhost/e-commerce-PHP/contact">Nous contacter</a></li>
-            <li><a href="http://localhost/e-commerce-PHP/register">Inscription</a></li>
+            <li><a href="http://localhost/e-commerce-PHP/login">Connexion</a></li>
         </ul>
     </div>
