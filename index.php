af63864e6e1097ce9868f04e4d8a3d04404f4ce2
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-02-24 22:44:04 +0100

    jonction nv front de sajed avec la bdd

--- index.php
+++ index.php
@@ -3,53 +3,125 @@
 session_start();
 
 ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>e-commerce PHP</title>
-    <!-- import CSS -->
-    <style>
-        <?php include("CSS/main.css") ?>
-    </style>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Prestige</title>
+    <link href="CSS/style.css" rel="stylesheet" id="css" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 
 <?php
 // Mise à jour des produits
 require_once('utils/generate_product_page.php'); ?>
 <script type="text/javascript">
-    function colorSwitch() {
-        var element = document.getElementById("landing-page");
-        if (element.classList.value === "landing-page-green"){
-            element.classList.replace("landing-page-green", "landing-page-grey")
-        }else{
-            element.classList.replace("landing-page-grey", "landing-page-green")
+    async function colorSwitch() {
+        console.log("CLICKED")
+        const css = document.getElementById("css");
+
+        const logo = document.getElementById("logo");
+        const watch = document.getElementById("watch");
+        const price = document.getElementById("price")
+
+        const left = document.getElementById("left-button")
+        const right = document.getElementById("right-button")
+
+        await new Promise(r => setTimeout(r, 1));
+        switch (window.location.hash) {
+            case "#patek":
+                logo.src = "assets/img/White_Prestige.png"
+                watch.src = "assets/img/watches/Capture d'écran 2024-02-23 151134.png"
+                css.href = "CSS/patek.css"
+                left.href = "#"
+                right.href = "#rolex"
+                price.innerHTML = "50.000 €"
+                break;
+            case "#daytona":
+                logo.src = "assets/img/White_Prestige.png"
+                watch.src = "assets/img/watches/rolex-pepsi (1).png"
+                css.href = "CSS/daytona.css"
+                left.href = "#rolex"
+                right.href = "#"
+                price.innerHTML = "20.000 €"
+                break;
+            case "#rolex":
+                logo.src = "assets/img/Black-Prestige.png"
+                watch.src = "assets/img/watches/Rolex.png"
+                css.href = "CSS/rolex.css"
+                left.href = "#patek"
+                right.href = "#daytona"
+                price.innerHTML = "30.000 €"
+                break;
+            default:
+                logo.src = "assets/img/Black-Prestige.png"
+                watch.src = "assets/img/watches/Watches.png"
+                css.href = "CSS/style.css"
+                left.href = "#daytona"
+                right.href = "#patek"
+                price.innerHTML = "25.000 €"
+                break;
+
         }
+
     }
 </script>
+
 <body>
-    <div class="landing-page-green" id="landing-page">
-        <?php require_once('navbar.php'); ?>
-        <!-- bouton de déconnexion (à mettre dans la navbar une fois faite) -->
-        <form action="utils/disconnect.php">
-            <input type="submit" name="disconnect" value="disconnect" />
-        </form>
+    <div class="container">
+        <header>
+            <nav class="navbar">
+                <a href="./"><img src="assets/img/Black-Prestige.png" alt="Logo Y’Cross" class="logo" id="logo" /></a>
+                <div class="right-menu">
+                    <a class="nav-link" href="products/products.html">Shop</a>
+                    <a class="nav-link" href="about.html">À Propos</a>
+                    <a class="nav-link" href="contact.html">Nous contacter</a>
+                    <?php if (@$_SESSION["user"]) {
+                        echo '<form action="utils/disconnect.php">
+                        <input type="submit" class="nav-link btn btn-dark text-white" name="disconnect" value="disconnect" />
+                    </form>';
+                    } else {
+                        echo '<a class="nav-link btn btn-dark text-white" href="login/">Connexion</a>';
+                    }
+                    ?>
 
-        <div class="title">
-            <h1>E-commerce php</h1>
+                </div>
+            </nav>
+        </header>
+
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Montre Exclusif</h1>
+                <p class="tagline">
+                    Choisissez le Luxe,<a class="text-black">Choisissez Nous.</a>
+                </p>
+                <p class="Bienvenue">
+                    Découvrez la Montre Parfaite pour Chaque Occasion et Élevez Votre
+                    Style avec une Élégance Intemporelle et un Savoir-Faire de
+                    Précision.
+                </p>
+                <div class="price-tag mb-4">
+                    <span class="price" id="price">25.000 €</span>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="carousel-controls">
+                    <a href="#daytona" class="rond" id="left-button" onclick="colorSwitch()">
+                        < </a>
+                            <img src="assets/img/watches/Watches.png" alt="watch" id="watch" class="img-fluid" />
+                            <!-- <button href="./rolex.html" class="rond">></></button> -->
+                            <a href="#patek" class="rond" id="right-button" onclick="colorSwitch()">></a>
+                </div>
+            </div>
         </div>
-        <?php
-        // Test session
-        if (@$_SESSION["user"]) {
-            echo "<h1>Hello " . $_SESSION["user"]["firstName"] . "</h1>";
-            echo bin2hex(random_bytes(16));
-        }
-        ?>
-        <button onclick="colorSwitch()">switch</button>
-
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 </body>
 
 
--- login/index.php
+++ login/index.php
@@ -54,42 +54,67 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        <?php include("../CSS/main.css") ?>
-    </style>
+    <link rel="stylesheet" href="../CSS/login.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
     <title>login</title>
 </head>
 
 
 <body>
-    <div class="login-page">
-        <div class="login-form-container">
-
+    <div class="container">
+        <header>
+            <nav class="navbar">
+                <a href="../">
+                    <img src="../assets/img/Violet-white-prestige.png" alt="Logo " class="logo" />
+                </a>
+                <div class="right-menu">
+                    <a class="nav-link" href="/products/products.html">Shop</a>
+                    <a class="nav-link" href="/about.html">À Propos</a>
+                    <a class="nav-link" href="/contact.html">Nous contacter</a>
+                    <a class="nav-link btn btn-dark text-white" href="/register/register.html">Inscription</a>
+                </div>
+            </nav>
+        </header>
+        <div class="login-container">
             <div class="login-form">
-                <span class="form-title">CONNEXION</span><br><br>
-                <span class="form-subtitle">Découvrez l'élégance et le "Prestige" chez nous.</span><br><br>
-                <form method="post">
-                    <input type="text" class="form-email" id="form-email" name="form-email"
-                        placeholder="Entrez votre mail"><br><br>
-
-                    <input type="password" class="form-password" id="form-password" name="form-password"
-                        placeholder="Mot de passe"><br><br>
-
-                    <button class="form-button">Connexion</button>
+                <h1>Connexion</h1>
+                <p>Découvrez l'élégance et le "Prestige" chez nous.</p>
+                <form method="POST">
+                    <label for="email"><img src="/assets/img/icon/user.png" alt="" /> Entrer votre
+                        Mail</label>
+                    <input type="email" id="email" name="form-email" required />
+                    <label for="password"><img src="/assets/img/icon/password1.png" alt="" /> Mot de
+                        passe</label>
+                    <input type="password" id="password" name="form-password" required />
+                    <button type="submit" class="btn-gradient">Connexion</button>
                 </form>
+                <div class="other-login">
+                    <p><a class="login-bold">Login</a> with Others</p>
+                    <button class="btn-gitea">
+                        <img src="/assets/img/icon/icons8-tea-24.png" alt="" />
+                        <a href="https://gitea.com/user/login">Connectez-vous avec Gitea</a>
+                    </button>
+                    <button class="btn-github">
+                        <img src="/assets/img/icon/icons8-github-32.png" alt="" /><a href="https://github.com/login">
+                            Connectez-vous avec Github</a>
+                    </button>
+                </div>
             </div>
-        </div>
-
-        <div class="login-presentation-container">
-
-            <div class="login-presentation">
-                <span class="login-presentation-text">Connectez-vous pour explorer l'exclusivité. Avec "Prestige",
-                    découvrez l'art de l'horlogerie de luxe, où chque montre raconte une histoire d'élégance
-                    intemporelle</span><br><br>
+            <div class="login-description">
+                <p>
+                    Connectez-vous pour explorer l'exclusivité. Avec "Prestige",
+                    découvrez l'art de l'horlogerie de luxe, où chaque montre raconte
+                    une histoire d'élégance intemporelle.
+                </p>
+                <div class="image-container">
+                    <img src="../assets/img/Icon_prestige.png" alt="Description de l'image" class="img-fluid" />
+                </div>
             </div>
         </div>
     </div>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
