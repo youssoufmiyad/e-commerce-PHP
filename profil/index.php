394cb32858f0122bf34045dc2c75296a38540992
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-02-24 23:23:48 +0100

    front profil

--- profil/index.php
+++ profil/index.php
@@ -4,10 +4,11 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>profil</title>
-    <style><?php include("../CSS/main.css") ?></style>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Votre Profil - Prestige</title>
+    <link rel="stylesheet" href="../CSS/profil.css" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 
 <?php
@@ -16,25 +17,48 @@ require_once('../utils/connect.php');
 ?>
 
 <body>
-    <?php require_once('../navbar.php'); ?>
-    <?php
-    try {
-        echo $_SESSION["user"]["userId"];
+    <div class="container">
+        <?php include("../navbar.php");
         $image = $db->query("SELECT Image FROM users_photo WHERE UserId=" . $_SESSION["user"]["userId"] . ";");
         $image = $image->fetch_assoc();
         $base64img = @base64_encode($image["Image"]);
-        $src = "data:image/jpg;base64," . $base64img;
-    } catch (\Throwable $th) {
-        throw $th;
-    }
+        $src = "data:image/jpg;base64," . $base64img; ?>
 
-    ?>
-    <!-- photo de profil -->
-    <img src="<?php echo $src ?>" alt="pfp" width="360px" height="360px">
-    <!-- prénom NOM -->
-    <div class="user-name">
-        <?php echo $_SESSION["user"]["lastName"] . " " . $_SESSION["user"]["firstName"] ?>
+        <div class="profile-container">
+            <h1>Votre Profil</h1>
+            <div class="profile-details">
+                <!-- photo de profil -->
+                <div class="profile-picture">
+                    <img src="<?php if ($src !== "data:image/jpg;base64,") {
+                        echo $src;
+                    } else {
+                        echo "./user-profile.png";
+                    } ?>" alt="Photo de profil" />
+                </div>
+                <div class="profile-info">
+                    <!-- prénom NOM -->
+                    <p><strong>Nom:</strong>
+                        <?php echo $_SESSION["user"]["lastName"] . " " . $_SESSION["user"]["firstName"] ?>
+                    </p>
+                    <p><strong>Email:</strong>
+                        <?php echo $_SESSION["user"]["email"] ?>
+                    </p>
+                    <p><strong>Date d'inscription:</strong> 20 janvier 2024</p>
+                    <p><strong>Dernière connexion:</strong> 5 février 2024</p>
+                </div>
+            </div>
+            <div class="profile-actions">
+                <form action="modify" id="modify"></form>
+                <form action="../utils/disconnect.php" id="disconnect"></form>
+                <button class="btn btn-primary" form="modify">Modifier le profil</button>
+                <button class="btn btn-danger" form="disconnect">Déconnexion</button>
+            </div>
+        </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
