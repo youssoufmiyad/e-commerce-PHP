c9a2292df23f4cf3453fbffe5ed5f7a6b080edde
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-02-25 23:19:53 +0100

    post d'article par l'utilisateur

--- profil/add product/index.php
+++ profil/add product/index.php
@@ -6,27 +6,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>add product</title>
-    <style><?php include("../../CSS/main.css") ?></style>
+    <style>
+        <?php include("../../CSS/main.css") ?>
+    </style>
 </head>
 <?php
 // Import de la connection à la database sous la forme de la variable "$db"
 require_once('../../utils/connect.php');
+ini_set("post_max_size", "30M");
+ini_set("upload_max_filesize", "30M");
+ini_set("memory_limit", "20000M");
+echo ini_get("upload_max_filesize");
 
 ?>
 <?php
 // Envoi du formulaire de création d'un produit
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     // Vérification du remplissage des champs coté serveur
-    if (!empty($_POST['product-name']) && !empty($_POST['price']) && !empty($_FILES['productImage'])) {
+    if (!empty($_POST['product-name']) && !empty($_POST['price'])) {
+        $ROOT = ".";
+        foreach ($_FILES as $file => $details) {   // Move each file from its temp directory to $ROOT
+            echo "tmp : " . $details['tmp_name'];
+            echo "\nname : " . $details['name'];
+            $temp = $details['tmp_name'];
+            $target = $details['name'];
+            move_uploaded_file($temp, $ROOT . '/' . $target);
+        }
+
         $name = $_POST['product-name'];
         $price = $_POST['price'];
+        $description = $_POST['descritpion'];
+        $category = $_POST['category'];
         $vendor = "NULL";
-        $quantity = 1;
-        $image = file_get_contents($_FILES['productImage']['tmp_name']);
+
+        $image = file_get_contents($_FILES['productImage']['name']);
+        $info = getimagesize($_FILES["productImage"]["name"]);
+        $extension = image_type_to_extension($info[2]);
 
         // Requête d'insert du produit à la base de données
-        $postQuery = $db->prepare("INSERT INTO `products` (`Name`, `Price`, `Vendor`, `Quantity`, `UserId`) VALUES (?,?,?,?,?);");
-        $postQuery->bind_param("ssssi", $name, $price, $vendor, $quantity, $_SESSION["user"]["userId"]);
+        $postQuery = $db->prepare("INSERT INTO `products` (`Name`, `Price`,`Description`, `Category`, `Vendor`, `Quantity`, `UserId`) VALUES (?,?,?,?,?,1,?);");
+        $postQuery->bind_param("sssssi", $name, $price, $description, $category, $vendor, $_SESSION["user"]["userId"]);
 
         try {
             // Execution de la requête
@@ -34,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             echo "Produit ajouté avec succès !";
 
             // Selection de l'ID du produit que l'on vient d'introduire, cette information servira pour l'ajout de l'image
-            $result = $db->query("SELECT ProductId from products WHERE Name='$name' AND Price=$price AND Vendor='$vendor' AND Quantity='$quantity'");
+            $result = $db->query("SELECT ProductId from products WHERE Name='$name' AND Price=$price AND UserId=".$_SESSION["user"]["userId"]);
             $actualProduct = $result->fetch_assoc();
 
             // Requête d'insert du produit à la base de données
@@ -42,6 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $addPhoto->bind_param("ss", $actualProduct["ProductId"], $image);
             $addPhoto->execute();
             echo "Photo ajouté avec succès !";
+            unlink($_FILES["productImage"]["name"]);
 
         } catch (Exception $e) {
             throw $e;
@@ -55,24 +75,35 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 ?>
 
 <body>
-<?php require_once('../../navbar.php'); ?>
+    <?php require_once('../../navbar.php'); ?>
     <div>
         <span>Ajout d'un produit</span>
+        <form method="post" enctype="multipart/form-data">
         <form method="post" enctype="multipart/form-data">
             <label for="product-name">Nom du produit :</label>
             <input type="text" name="product-name" id="product-name" placeholder="entrez un nom">
             <br>
 
+
             <label for="price">Prix :</label>
             <input type="number" step="any" name="price" id="price" placeholder="entrez un prix">
             <br>
-            <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+
+            <label for="descritpion">Description :</label>
+            <input type="text" name="descritpion" id="descritpion" placeholder="entrez une descritpion">
+            <br>
+
+            <label for="category">Category :</label>
+            <input type="text" name="category" id="category" placeholder="montre en or, acier, de sport, etc...">
+            <br>
+
+            <input type="hidden" name="MAX_FILE_SIZE" value="999999999999" />
 
             <label for="productImage">Image</label>
             <input type="file" name="productImage" id="productImage" accept="image/png, image/gif, image/jpeg"><br>
 
 
-            <button>postez</button>
+            <button>Poster</button>
         </form>
     </div>
 </body>
--- profil/index.php
+++ profil/index.php
@@ -167,63 +167,110 @@ require_once('../utils/connect.php');
                 ?>
             </div>
         </div>
-        <div class="profile-container">
-        <h1>Commandes :</h1>
 
+        <div class="profile-container">
+            <h1>Produit :</h1>
             <?php
             // Test si l'utilisateur est connecté
             if (@$_SESSION["user"]) {
-                $orders = $db->query('SELECT * FROM orders WHERE UserId=' . $_SESSION["user"]["userId"] . ';');
+                $products = $db->query('SELECT * FROM products WHERE UserId=' . $_SESSION["user"]["userId"] . ';');
                 // Test si l'utilisateur a déjà commandé
-                if ($orders->num_rows > 0) {
+                if ($products->num_rows > 0) {
                     ?>
                     <!-- Affichage de chaque produit -->
                     <div class="order-list">
                         <?php
-                        foreach ($orders as $order) {
-                            $items = $db->query('SELECT * FROM order_items WHERE OrderId=' . $order['OrderId'] . ';');
-                            ?>
-                            <div class="orders-item" id="order-<?= $order['OrderId'] ?>">
-                                <h1>ORDER
-                                    <?= $order['OrderId'] ?>
-                                </h1>
-                                <?php
-                                foreach ($items as $item) {
-                                    $item_info = $db->query('SELECT * FROM products WHERE ProductId=' . $item['ProductId'] . ';')->fetch_assoc();
-                                    ?>
-                                    <!-- TODO : 
-                    - ajouter les champs propre aux commandes tels que la date d'envoi/livraison
--->
+                        foreach ($products as $product) {
+                            $product_image = $db->query("SELECT Image FROM products_photo WHERE ProductId=" . $product['ProductId'] . ";");
+                            $product_image = $product_image->fetch_assoc();
+                            $base64productimg = @base64_encode($product_image["Image"]);
+                            $src = "data:image/jpg;base64," . $base64productimg; ?>
 
-                                    <div class="product-name">
-                                        <?= $item_info['Name'] ?>
-                                    </div>
-
-                                    <?php
-                                } ?>
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="<?= $src ?>" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        <?= $product['Name'] ?>
+                                    </h5>
+                                    <p class="card-text">
+                                        <?= $product['Price'] ?> €.
+                                    </p>
+                                    <a href="../product/<?= $product['ProductId'] ?>" class="btn btn-primary">Voir</a>
+                                </div>
                             </div>
-                            <form action="../cart/checkout/invoice/index.php" method="post">
-                                <input type="hidden" name="order-id" value="<?= $order['OrderId'] ?>">
-                                <button>voir facture</button>
-                            </form>
                             <br>
                             <?php
                         }
 
                 } else {
-                    echo "<h3>Vous n'avez passez aucune commande</h3>";
-                }
+                    echo "<h3>Vous n'avez vendu aucun article</h3>";
+                } ?>
+                    <a href="add product" class="btn btn-primary">Nouvelle article</a>
+                    <?php
 
             } else {
-                echo "connectez vous pour voir votre historique de commande";
+                echo "connectez vous pour voir vos produits mis en vente";
             }
             ?>
             </div>
-        </div>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+            <div class="profile-container">
+                <h1>Commandes :</h1>
+
+                <?php
+                // Test si l'utilisateur est connecté
+                if (@$_SESSION["user"]) {
+                    $orders = $db->query('SELECT * FROM orders WHERE UserId=' . $_SESSION["user"]["userId"] . ';');
+                    // Test si l'utilisateur a déjà commandé
+                    if ($orders->num_rows > 0) {
+                        ?>
+                        <!-- Affichage de chaque produit -->
+                        <div class="order-list">
+                            <?php
+                            foreach ($orders as $order) {
+                                $items = $db->query('SELECT * FROM order_items WHERE OrderId=' . $order['OrderId'] . ';');
+                                ?>
+                                <div class="orders-item" id="order-<?= $order['OrderId'] ?>">
+                                    <h1>ORDER
+                                        <?= $order['OrderId'] ?>
+                                    </h1>
+                                    <?php
+                                    foreach ($items as $item) {
+                                        $item_info = $db->query('SELECT * FROM products WHERE ProductId=' . $item['ProductId'] . ';')->fetch_assoc();
+                                        ?>
+                                        <!-- TODO : 
+                    - ajouter les champs propre aux commandes tels que la date d'envoi/livraison
+-->
+
+                                        <div class="product-name">
+                                            <?= $item_info['Name'] ?>
+                                        </div>
+
+                                        <?php
+                                    } ?>
+                                </div><br>
+                                <form action="../cart/checkout/invoice/index.php" method="post">
+                                    <input type="hidden" name="order-id" value="<?= $order['OrderId'] ?>">
+                                    <button class="btn btn-primary">voir facture</button>
+                                </form>
+                                <br>
+                                <?php
+                            }
+
+                    } else {
+                        echo "<h3>Vous n'avez passez aucune commande</h3>";
+                    }
+
+                } else {
+                    echo "connectez vous pour voir votre historique de commande";
+                }
+                ?>
+                </div>
+            </div>
+
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
