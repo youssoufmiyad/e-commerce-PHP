def3fce23a29bbbf0f367141bc0992e7a429bd2d
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-02-25 11:42:35 +0100

    modification d'infos front

--- profil/modify/index.php
+++ profil/modify/index.php
@@ -9,7 +9,8 @@ require_once('../../utils/connect.php'); ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style><?php include("../../CSS/main.css") ?></style>
+    <link rel="stylesheet" href="../../CSS/profil.css" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
     <title>modify</title>
 </head>
 
@@ -45,28 +46,31 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 
 <body>
-<?php require_once('../../navbar.php'); ?>
-    <span>Informations du compte</span>
-    <form method="post" action="change-info.php">
-        <label for="form-lastname">Last name:</label><br>
-        <input type="text" id="form-lastname" name="form-lastname" value=<?php echo $_SESSION["user"]["lastName"] ?>><br><br>
-        <label for="form-firstname">First name:</label><br>
-        <input type="text" id="form-firstname" name="form-firstname" value=<?php echo $_SESSION["user"]["firstName"] ?>><br>
-        <label for="form-email">Email:</label><br>
-        <input type="text" id="form-email" name="form-email" value=<?php echo $_SESSION["user"]["email"] ?>><br><br>
-        <input type="submit" value="Submit"><br><br>
-    </form>
+    <?php require_once('../../navbar.php'); ?>
+    <div class="form-container">
 
-    <span>Mot de passe</span>
-    <form method="post" action="change-password.php">
-        <label for="form-old-password">Old password:</label><br>
-        <input type="password" id="form-old-password" name="form-old-password"><br><br>
-        <label for="form-password">New password:</label><br>
-        <input type="password" id="form-password" name="form-password"><br><br>
-        <label for="form-confirmPWD">Confirm password:</label><br>
-        <input type="password" id="form-confirmPWD" name="form-confirmPWD"><br><br>
-        <input type="submit" value="Submit">
-    </form>
+        <form method="post" action="change-info.php">
+            <span><strong>Informations du compte:</strong></span><br>
+            <label for="form-lastname">Last name:</label><br>
+            <input type="text" id="form-lastname" name="form-lastname" value=<?php echo $_SESSION["user"]["lastName"] ?>><br><br>
+            <label for="form-firstname">First name:</label><br>
+            <input type="text" id="form-firstname" name="form-firstname" value=<?php echo $_SESSION["user"]["firstName"] ?>><br>
+            <label for="form-email">Email:</label><br>
+            <input type="text" id="form-email" name="form-email" value=<?php echo $_SESSION["user"]["email"] ?>><br><br>
+            <input type="submit" value="Submit"><br><br>
+        </form>
+
+        <form method="post" action="change-password.php">
+            <span><strong>Mot de passe:</strong></span><br>
+            <label for="form-old-password">Old password:</label><br>
+            <input type="password" id="form-old-password" name="form-old-password"><br><br>
+            <label for="form-password">New password:</label><br>
+            <input type="password" id="form-password" name="form-password"><br><br>
+            <label for="form-confirmPWD">Confirm password:</label><br>
+            <input type="password" id="form-confirmPWD" name="form-confirmPWD"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
 </body>
 
 </html>
